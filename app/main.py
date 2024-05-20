6877e7fbe6984705a146ebc8ae8043d851d2c693
christopher shih <chrshih875@gmail.com>
2024-05-20 14:27:25 -0700

    stage 2

--- app/main.py
+++ app/main.py
@@ -7,7 +7,9 @@ def main():
 
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, address = server_socket.accept() # wait for client
+    conn.send(b"+PONG\r\n")
+    print("con", conn)
 
 
 if __name__ == "__main__":
