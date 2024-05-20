7533238d85e38b460f338413a1d14e7698492947
christopher shih <chrshih875@gmail.com>
2024-05-20 15:24:33 -0700

    finally work?

--- app/main.py
+++ app/main.py
@@ -8,8 +8,13 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, address = server_socket.accept() # wait for client
-    while True:
-        conn.send(b"+PONG\r\n")
+    with conn:
+        while True:
+            print("Waiting for message")
+            data = conn.recv(4096).decode()
+            print(data)
+            conn.send("+PONG\r\n".encode())
+            print("Sent message")
     print("con", conn)
 
 
