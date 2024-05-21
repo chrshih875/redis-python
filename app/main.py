a4adb16d1f33a53e57a9e55627c0f0beee198962
root <root@DESKTOP-5PHEJIR>
2024-05-21 14:13:25 -0700

    implement echo command done

--- app/main.py
+++ app/main.py
@@ -1,33 +1,43 @@
 import socket
-import threading
+from threading import *
 
-def handle_request(connection, address):
-    with connection:
+class Connection(Thread):
+    def __init__(self, socket, address):
+        super().__init__()
+        self.socket = socket
+        self.address = address
+        self.start()
+
+    def run(self):
         while True:
-            print("Waiting for message")
-            data = connection.recv(4096).decode()
-            if not data:
-                break
-            print("DATA", data)
-            command = data[0].upper()
-            match command:
-                case "PING":
-                    connection.send("+PONG\r\n".encode())
-                case ECHO:
-                    connection.send(data[1].encode())
+            # print("Waiting for message")
+            request = self.socket.recv(4096)
+            if request:
+                parse_request = self.parse_request(request)
+                # print("PARSE_REQ", parse_request) 
+                self.parse_command(parse_request)
 
-            print("Sent message")
+    def parse_request(self, request):
+        return request.decode().split("\r\n")[2:-1:2]
 
+    def parse_command(self, command):
+        check_command = command[0].upper()
+        match check_command:
+            case "PING":
+                self.socket.send("+PONG\r\n".encode())
+            case ECHO:
+                self.socket.send(f"+{command[1]}\r\n".encode())
+        print("Sent message")
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    print("testing this commint went through")
     while True:
         conn, address = server_socket.accept() # wait for client
-        threading.Thread(target=handle_request, args=(conn, address)).start()
+        Connection(conn, address)
+        # threading.Thread(target=handle_request, args=(conn, address)).start()
 
 
 
