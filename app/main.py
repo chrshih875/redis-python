7269bf1605345900cb40171978481d7012a1abc3
christopher shih <chrshih875@gmail.com>
2024-05-20 19:01:27 -0700

    handling concurrent cilents 9 it needs it

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 
-def handle_request(connection):
+def handle_request(connection, address):
     with connection:
         while True:
             print("Waiting for message")
@@ -20,7 +20,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     while True:
         conn, address = server_socket.accept() # wait for client
-        threading.Thread(target=handle_request, args=(conn)).start()
+        threading.Thread(target=handle_request, args=(conn, address)).start()
 
 
 
