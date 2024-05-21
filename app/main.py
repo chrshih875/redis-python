467760a4b7fb5e83f333b11197346bac428c1dbc
christopher shih <chrshih875@gmail.com>
2024-05-20 18:49:01 -0700

    Handle concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import socket
-
+import threading
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,14 +8,16 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     conn, address = server_socket.accept() # wait for client
-    with conn:
+    threading.Thread(target=handle_request, args=conn).start()
+
+def handle_request(connection):
+    with connection:
         while True:
             print("Waiting for message")
-            data = conn.recv(4096).decode()
+            data = connection.recv(4096).decode()
             print(data)
-            conn.send("+PONG\r\n".encode())
+            connection.send("+PONG\r\n".encode())
             print("Sent message")
-    print("con", conn)
 
 
 if __name__ == "__main__":
