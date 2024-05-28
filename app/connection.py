7b24131c3164fdef8770db2422c1fe6ae9465ce5
chrshih875 <chrshih875@gmail.com>
2024-05-28 16:46:23 -0700

    empty RDB transfer

--- app/connection.py
+++ app/connection.py
@@ -110,4 +110,8 @@ class Commands(Thread, RDB_fileconfig, Streams):
                 self.socket.send("+OK\r\n".encode())
             case "PSYNC":
                 self.socket.send("+FULLRESYNC 8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb 0\r\n".encode())
+                rdb_hex = '524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2'
+                rdb_content = bytes.fromhex(rdb_hex)
+                rdb_length = f"${len(rdb_content)}\r\n".encode()
+                self.socket.send(rdb_length+rdb_content)
         print("Sent message")
