3fda653ef1088789b49e086ab3fc7f2c5c9655d6
chrshih875 <chrshih875@gmail.com>
2024-05-29 20:11:01 -0700

    ACKs with no command

--- app/main.py
+++ app/main.py
@@ -38,15 +38,6 @@ def main():
         master_conn = socket.socket()
         master_conn.connect((master_host, int(master_port)))
         server.connect_master(master_conn, args.port)
-        # print("master_thread1111")
-        # master_thread = threading.Thread(
-        #     target=server.recieve_command,
-        #     args=(master_conn,),
-        #     daemon=True,
-        # ).start()
-        # server.recieve_command(master_conn)
-        # master_conn.close()
-    # else:
     while True:
         conn, address = server_socket.accept() # wait for client
         Commands(conn, address, args.dir, args.dbfilename, shared_data, replication, args.replicaof, server, role=role)
--- app/replication.py
+++ app/replication.py
@@ -15,6 +15,7 @@ class Replication:
     
     def connect_master(self, conn, slave_port):
         # with socket.create_connection((host, port)) as s:
+        # testing = ["*1\r\n$4\r\nPING\r\n".encode(), f"*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n{slave_port}\r\n".encode(), "*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n".encode(), "*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n".encode()]
         try:
             conn.send("*1\r\n$4\r\nPING\r\n".encode())
             conn.recv(1024)
@@ -24,7 +25,28 @@ class Replication:
             conn.recv(1024)
             conn.send("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n".encode())
             conn.recv(1024)
-            data = conn.recv(1024)
+            conn.recv(1024)
+            # while sig := conn.recv(1024):
+            #     print("sigggggggg", sig)
+            #     parsed = self.parse_request(sig)    
+            #     print("PARRRRRRRRRR", parsed)                
+            # for co in range(3):
+            # conn.recv(1024)
+                
+            # data = conn.recv(1024)
+            # print("DATTTA", data)
+            # request = conn.recv(1024)
+            # print("req", request)
+            # if request:
+            #     hello = self.parse_request(request)
+            #     print("HELLLLLLLLLLO", hello)
+            #     if hello[0][0] == "SET":
+            #         print("SETTT")
+            #         self.set[hello[0][2]] = hello[0][-1]
+            #     else:
+            #         print("ELSEEE")
+            #         conn.send("*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n0\r\n".encode())
+            print("DID NTO REQUESt")
             master_thread = threading.Thread(
             target=self.recieve_command,
             args=(conn,),
@@ -34,29 +56,32 @@ class Replication:
             print(f"An error occurred: {e}")
 
     def recieve_command(self, conn):
+        print("not hit")
         while True:
             request = conn.recv(1024)
             if request:
                 command = self.parse_request(request)
+                print("command", command)
                 self.parse_command(command, request, conn)
 
     def parse_request(self, data):
         array = []
         current_data = data.decode().split("\r\n")[2:-1:1]
+        print("current_dayta", current_data)
         for i in range(len(current_data)):
             if current_data[i] == "SET":
+                array.append(current_data[i:i+5])
+            if current_data[i] == "REPLCONF":
                 array.append(current_data[i:i+5])
         return array
     
     def parse_command(self, command, request, master_conn):
         for val in command:
-            # check_command = val[0].upper()
-            # match check_command:
-            #     case "SET":
-            #             print("SETTTT")
-            self.set[val[2]] = val[-1]
+            if val[0] == "SET":
+                self.set[val[2]] = val[-1]
+            if val[0] == "REPLCONF":
+                master_conn.send("*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n0\r\n".encode())
             master_conn.send("+OK\r\n".encode())
-                        # master_conn.send(f"$3\r\n123\r\n".encode())
         
     def empty_RDB(self):
         rdb_hex = '524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2'
